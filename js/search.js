a8403854adba54c8ae8a3f52012f1b6846b0ccca
Neelabh Pandey <incore.labs.np@gmail.com>
2015-02-21 10:27:41 +0530

    updated search and icons. added senator.

--- js/search.js
+++ js/search.js
@@ -29,8 +29,11 @@ var searchMiniApp = {
         var memberOccupation = $("#memberOccupation").val();
         var queryString = [];
         queryString[0] = "SELECT id, Name FROM male WHERE";
-        queryString[1] = "SELECT id, Name FROM female WHERE";
-        queryString[2] = "SELECT id, Name FROM kids WHERE";
+        queryString[1] = "SELECT id, Name FROM s_male WHERE";
+        queryString[2] = "SELECT id, Name FROM female WHERE";
+        queryString[3] = "SELECT id, Name FROM s_female WHERE";
+        queryString[4] = "SELECT id, Name FROM kids WHERE";
+        queryString[5] = "SELECT id, Name FROM s_kids WHERE";
         var nameQueryString = "Name LIKE '%"+memberName+"%'";
         var bloodQueryString = "Blood_Group = '"+bloodGroup+"'";
         var occupationQueryString = "Occupation LIKE '%"+memberOccupation+"%'";
@@ -38,14 +41,23 @@ var searchMiniApp = {
             queryString[0] += " " + nameQueryString;
             queryString[1] += " " + nameQueryString;
             queryString[2] += " " + nameQueryString;
+            queryString[3] += " " + nameQueryString;
+            queryString[4] += " " + nameQueryString;
+            queryString[5] += " " + nameQueryString;
             if(activeFields[1]) {
                 queryString[0] += " AND "+bloodQueryString;
                 queryString[1] += " AND "+bloodQueryString;
                 queryString[2] += " AND "+bloodQueryString;
+                queryString[3] += " AND "+bloodQueryString;
+                queryString[4] += " AND "+bloodQueryString;
+                queryString[5] += " AND "+bloodQueryString;
                 if(activeFields[2]) {
                     queryString[0] += " AND "+occupationQueryString;
                     queryString[1] += " AND "+occupationQueryString;
+                    queryString[2] += " AND "+occupationQueryString;
+                    queryString[3] += " AND "+occupationQueryString;
                     queryString.pop();                                  //   Kids have no occupation.
+                    queryString.pop();                                  //   s_Kids have no occupation.
                 }
             }
         }
@@ -53,21 +65,129 @@ var searchMiniApp = {
             queryString[0] += " " + bloodQueryString;
             queryString[1] += " " + bloodQueryString;
             queryString[2] += " " + bloodQueryString;
+            queryString[3] += " " + bloodQueryString;
+            queryString[4] += " " + bloodQueryString;
+            queryString[5] += " " + bloodQueryString;
             if(activeFields[2]) {
                 queryString[0] += " AND "+occupationQueryString;
                 queryString[1] += " AND "+occupationQueryString;
+                queryString[2] += " AND "+occupationQueryString;
+                queryString[3] += " AND "+occupationQueryString;
                 queryString.pop();                                      //   Kids have no occupation.
+                queryString.pop();                                  //   s_Kids have no occupation.
             }
         }
         else if(activeFields[2]) {
             queryString[0] += " " + occupationQueryString;
             queryString[1] += " " + occupationQueryString;
+            queryString[2] += " " + occupationQueryString;
+            queryString[3] += " " + occupationQueryString;
             queryString.pop();                                          //   Kids have no occupation.
+            queryString.pop();                                          //   s_Kids have no occupation.
         }
         var k = 0;
         var resultsID = [];
         var resultsName = [];
-        app.db.transaction(function (tx) {
+
+        var maleResults = {id:[], name:[]};
+        var sMaleResults = {id:[], name:[]};
+        var femaleResults = {id:[], name:[]};
+        var sFemaleResults = {id:[], name:[]};
+        var kidsResults = {id:[], name:[]};
+        var sKidsResults = {id:[], name:[]};
+
+        app.db.transaction(function(tx){
+            var defMale = $.Deferred();
+            var defSMale = $.Deferred();
+            var defFemale = $.Deferred();
+            var defSFemale = $.Deferred();
+            var defKids = $.Deferred();
+            var defSKids = $.Deferred();
+            tx.executeSql(queryString[0],[],
+                function(tx, r){
+                    for(var j =0; j< r.rows.length; j++) {
+                        maleResults.id[j] = r.rows.item(j).id;
+                        maleResults.name[j] = r.rows.item(j).Name;
+                    }
+                    defMale.resolve();
+                },
+                app.dbQueryError
+            );
+            tx.executeSql(queryString[1],[],
+                function(tx, r){
+                    for(var j =0; j< r.rows.length; j++) {
+                        sMaleResults.id[j] = r.rows.item(j).id;
+                        sMaleResults.name[j] = r.rows.item(j).Name;
+                    }
+                    defSMale.resolve();
+                },
+                app.dbQueryError
+            );
+            tx.executeSql(queryString[2],[],
+                function(tx, r){
+                    for(var j =0; j< r.rows.length; j++) {
+                        femaleResults.id[j] = r.rows.item(j).id;
+                        femaleResults.name[j] = r.rows.item(j).Name;
+                    }
+                    defFemale.resolve();
+                },
+                app.dbQueryError
+            );
+            tx.executeSql(queryString[3],[],
+                function(tx, r){
+                    for(var j =0; j< r.rows.length; j++) {
+                        sFemaleResults.id[j] = r.rows.item(j).id;
+                        sFemaleResults.name[j] = r.rows.item(j).Name;
+                    }
+                    defSFemale.resolve();
+                },
+                app.dbQueryError
+            );
+
+            if(queryString.length > 4) {
+                tx.executeSql(queryString[4],[],
+                    function(tx, r){
+                        for(var j =0; j< r.rows.length; j++) {
+                            kidsResults.id[j] = r.rows.item(j).id;
+                            kidsResults.name[j] = r.rows.item(j).Name;
+                        }
+                        defKids.resolve();
+                    },
+                    app.dbQueryError
+                );
+                tx.executeSql(queryString[5],[],
+                    function(tx, r){
+                        for(var j =0; j< r.rows.length; j++) {
+                            sKidsResults.id[j] = r.rows.item(j).id;
+                            sKidsResults.name[j] = r.rows.item(j).Name;
+                        }
+                        defSKids.resolve();
+                    },
+                    app.dbQueryError
+                );
+                $.when(defMale, defSMale, defFemale, defSFemale, defKids, defSKids).done(function() {
+                    console.log("all done");
+                    console.log("male", maleResults.id, maleResults.name);
+                    console.log("female", femaleResults.id, femaleResults.name);
+                    console.log("kids", kidsResults.id, kidsResults.name);
+                    console.log("s_male", sMaleResults.id, sMaleResults.name);
+                    console.log("s_female", sFemaleResults.id, sFemaleResults.name);
+                    console.log("s_kids", sKidsResults.id, sKidsResults.name);
+                });
+            } else {
+                $.when(defMale, defSMale, defFemale, defSFemale).done(function() {
+                    console.log("all done");
+                    console.log("male", maleResults.id, maleResults.name);
+                    console.log("female", femaleResults.id, femaleResults.name);
+                    console.log("kids", kidsResults.id, kidsResults.name);
+                    console.log("s_male", sMaleResults.id, sMaleResults.name);
+                    console.log("s_female", sFemaleResults.id, sFemaleResults.name);
+                    console.log("s_kids", sKidsResults.id, sKidsResults.name);
+                });
+
+            }
+        });
+        /*app.db.transaction(function (tx) {
             tx.executeSql(queryString[0], [],
                 function(tx, r) {
                     for(var j =0; j< r.rows.length; j++) {
@@ -107,7 +227,7 @@ var searchMiniApp = {
                 },
                 app.dbQueryError
             );
-        });
+        });*/
     },
     buildSearchResults: function (k, resultsID, resultsName) {
         var searchConcatString = "";
